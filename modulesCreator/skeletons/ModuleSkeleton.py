5a1f5d4ddbd1488aa5ec585ff3e5aede5ccb20c4
Fabio Fiordalice <jimbo.boss.1990@gmail.com>
2018-10-27 18:28:11 +0100

    update

--- modulesCreator/skeletons/ModuleSkeleton.py
+++ modulesCreator/skeletons/ModuleSkeleton.py
@@ -19,11 +19,28 @@ class ModuleSkeleton(ModuleCreator):
 
         print list
 
+        jointList = []
+        chainFather = ''
+
         for i in range(0, len(list)):
             positionLocs = pm.pointPosition(list[i], w=True)
             nameLoc = list[i]
             subName = nameLoc[0: len(list[i]) - 3]
             nameJnt = subName + 'JNT'
             pm.joint(p=(positionLocs[0], positionLocs[1], positionLocs[2]), n=nameJnt)
-
+            jointList.append(nameJnt)
             pm.parent(nameJnt, w=True)
+            pm.setAttr(nameJnt+'.radius', 0.5)
+
+        for i in range(0, len(list)):
+            obj = pm.getAttr(list[i]+'.locFather')
+            print '\n\r Loc Father: '+obj+'\n\r'
+            if obj != 'self':
+                subName = obj[0: len(obj) - 3]
+                print '\n\r SubName: ' + subName + '\n\r'
+                jointFather = subName + 'JNT'
+                pm.parent(jointList[i], jointFather)
+            else:
+                chainFather = jointList[i]
+
+        pm.joint(chainFather, e=True, oj='yxz', secondaryAxisOrient='zup', ch=True, zso=True);
