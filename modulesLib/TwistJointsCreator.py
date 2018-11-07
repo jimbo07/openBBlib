7511be4894b6dfae3e4745472211c6e7c69bca6a
Fabio Fiordalice <jimbo.boss.1990@gmail.com>
2018-11-07 23:56:38 +0000

    update

--- modulesLib/TwistJointsCreator.py
+++ modulesLib/TwistJointsCreator.py
@@ -1,6 +1,6 @@
 import pymel.core as pm
 
-class TwistJointCreator:
+class TwistJointCreator(ModuleSkeleton):
 
     def __init__(self, objName, jointsNumber, groupJnts, curve, startJoint):
 
@@ -12,19 +12,29 @@ class TwistJointCreator:
 
         motionPathList = []
         #curveSpans = pm.getAttr(self.curve + 'Shape.spans')
-        pm.rebuildCurve(self.curve, ch=True, rt=0, rpo=True, end=1, kr=0, kep=True, kt=0, d=3, s=4)
-
-
+        if (self.curve == '') or (pm.objExists(self.curve) == False):
+            childStartJoint = pm.listRelatives(self.startJoint)
+            posStart = pm.xform(self.startJoint, q=True, t=True, ws=True)
+            posEnd = pm.xform(childStartJoint, q=True, t=True, ws=True)
+            autoGenCv = pm.curve( n=self.startJoint+'_twistJointsChain_CRV', d=1, p=[posStart, posEnd] )
+            self.curve = autoGenCv
+            pm.rebuildCurve(self.curve, ch=True, rt=0, rpo=True, end=1, kr=0, kep=True, kt=0, d=3, s=4)
+        else:
+            pm.rebuildCurve(self.curve, ch=True, rt=0, rpo=True, end=1, kr=0, kep=True, kt=0, d=3, s=4)
+        
+        pm.skinCluster(self.startJoint, childStartJoint, self.curve)
+        pm.select(cl=True)
+        
+        groupTwistJoints = pm.group(n=self.groupJnts)
+        
+        nameTwistJoint = self.startJoint[0:len(self.startJoint)-4]
+        
         for i in range(0, self.jointsNumber):
-            newJnt = pm.joint(n='twist_' + str(i + 1) + '_JNT', radius=0.25)
+            newJnt = pm.joint(n=nameTwistJoint+'_twist_' + str(i + 1) + '_JNT', radius=0.25)
             pm.setAttr(newJnt + '.overrideEnabled', 1)
             pm.setAttr(newJnt + '.overrideColor', 13)
 
-            if pm.uniqueObjExists(self.groupJnts):
-                pm.parent(newJnt, self.groupJnts)
-            else:
-                groupTwistJoints = pm.group(n=self.groupJnts)
-
+            pm.parent(newJnt, groupTwistJoints)
 
             # motionPath = pm.pathAnimation(newJnt, c=self.curve, fractionMode = 0, eu = 1)
             motionPath = pm.createNode('motionPath')
@@ -43,7 +53,7 @@ class TwistJointCreator:
             pm.disconnectAttr(newJnt + '.tz')
 
         jointlist = pm.listRelatives(self.groupJnts, c=True)
-        print list
+        #print list
         for i in range(0, len(jointlist)):
             if i == 0:
                 continue
@@ -54,7 +64,7 @@ class TwistJointCreator:
         pm.joint(jointlist[len(jointlist) - 1], e=True, oj='none', ch=True, zso=True)
 
         for i in range(1, len(jointlist)):
-            pm.parent(jointlist[i], self.groupJnts)
+            pm.parent(jointlist[i], groupTwistJoints)
 
         for i in range(0, len(motionPathList)):
             pm.connectAttr(motionPathList[i] + '.xCoordinate', jointlist[i] + '.tx', f=True)
